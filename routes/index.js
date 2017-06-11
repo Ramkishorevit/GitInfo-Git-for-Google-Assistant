cc3c05a97d95faf53e2b74e718299e99223f0e26
Ramkishorevit <ramkishorevit@gmail.com>
2017-06-11 12:13:22 +0530

    Sample payload

--- routes/index.js
+++ routes/index.js
@@ -45,22 +45,44 @@ function responseHandler (app) {
         break;
 
     case REPO_NAME:
-         let repo = app.getRawInput();    
-          app.ask(app.buildRichResponse()
-    // Create a basic card and add it to the rich response
-    .addSimpleResponse(repo)
-    .addBasicCard(app.buildBasicCard(`42 is an even composite number. It 
-      is composed of three distinct prime numbers multiplied together. It 
-      has a total of eight divisors. 42 is an abundant number, because the 
-      sum of its proper divisors 54 is greater than itself. To count from 
-      1 to 42 would take you about twenty-one…`)
-      .setTitle('Math & prime numbers')
-      .addButton('Read more')
-    )
-  );
+          list(app);
           break;
 
   }
 }
+
+function list (app) {
+  app.askWithList(app.buildRichResponse()
+    .addSimpleResponse(app.getRawInput())
+    .addSuggestions(
+      ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+    // Build a list
+    app.buildList('Things to learn about')
+    // Add the first item to the list
+    .addItems(app.buildOptionItem('MATH_AND_PRIME',
+      ['math', 'math and prime', 'prime numbers', 'prime'])
+      .setTitle('Math & prime numbers')
+      .setDescription('42 is an abundant number because the sum of its ' +
+        'proper divisors 54 is greater…')
+      .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
+    // Add the second item to the list
+    .addItems(app.buildOptionItem('EGYPT',
+      ['religion', 'egpyt', 'ancient egyptian'])
+      .setTitle('Ancient Egyptian religion')
+      .setDescription('42 gods who ruled on the fate of the dead in the ' +
+        'afterworld. Throughout the under…')
+      .setImage('http://example.com/egypt', 'Egypt')
+    )
+    // Add third item to the list
+    .addItems(app.buildOptionItem('RECIPES',
+      ['recipes', 'recipe', '42 recipes'])
+      .setTitle('42 recipes with 42 ingredients')
+      .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
+        'of flavor! All you need is some ginger and…')
+      .setImage('http://example.com/recipe', 'Recipe')
+    )
+  );
+}
+
 
 module.exports = router;
